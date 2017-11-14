5776b24fe16b91ba955a8a793b90c7a7525713fa
Taylor Donlan <taylordonlan@gmail.com>
2017-11-14 19:02:34 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,6 +12,6 @@ def input_to_index(input)
   return(input.to_i - 1)
 end
 
-def move(board, input_to_index, char = "X")
-  board[input_to_index] = char
+def move(board, index, char = "X")
+  board[index] = char
 end
